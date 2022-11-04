c0dc55bd380833dfa9c0e4964b5597263d28422b
Ivan Pomelnikov <vanidzepomelnikov@gmail.com>
2022-11-04 17:12:47 +0400

    Some corrections

--- SPO_7thSemester/src/main/java/Index.java
+++ SPO_7thSemester/src/main/java/Index.java
@@ -1,7 +1,8 @@
 public class Index {
 
     public static void main(String[] args) {
-
+        String upBookshelfFileName = "UpBookshelf.xml";
+        String midBookshelfFileName = "MidBookshelf.xml";
+        String downBookshelfFileName = "DownBookshelf.xml";
     }
-
 }
